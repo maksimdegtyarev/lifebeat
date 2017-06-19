0a70e140bd04d7d24a4d394c7ec5c8eb3a36f656
Maksim Degtyarev <brokkoli08@gmail.com>
2017-06-19 20:01:54 +0300

    hotfix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import {createStore, applyMiddleware} from 'redux'
-import thunkMiddleware from 'redux-thunk'
 import {logger} from 'redux-logger'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import eventsReducer from './events'
 
 
-export default createStore(eventsReducer, composeWithDevTools(applyMiddleware(thunkMiddleware), applyMiddleware(logger)))
+export default createStore(eventsReducer, composeWithDevTools(applyMiddleware(logger)))
